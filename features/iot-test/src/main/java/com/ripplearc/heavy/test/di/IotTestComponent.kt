a2b86ba842b61488f3654f9157be16c7fb374eb3
Suyang Dong <sdong13@ford.com>
2020-02-26 17:03:43 -0500

    - WIP

--- features/iot-test/src/main/java/com/ripplearc/heavy/test/di/IotTestComponent.kt
+++ features/iot-test/src/main/java/com/ripplearc/heavy/test/di/IotTestComponent.kt
@@ -1,12 +1,16 @@
 package com.ripplearc.heavy.iot.test.di
 
 import com.ripplearc.heavy.common.features.IotTestFeature
+import com.ripplearc.heavy.radio.di.AwsModule
+import com.ripplearc.heavy.radio.messaging.MessagingJob
 import com.ripplearc.heavy.test.ui.RequestActivity
 import dagger.BindsInstance
 import dagger.Component
+import javax.inject.Named
 
 @IotTestScope
 @Component(
+    modules = [AwsModule::class],
     dependencies = [IotTestFeature.Dependencies::class]
 )
 interface IotTestComponent {
@@ -14,10 +18,16 @@ interface IotTestComponent {
     interface Factory {
         fun create(
             dependencies: IotTestFeature.Dependencies,
-            @BindsInstance iotTestFeature: IotTestFeature
+            @BindsInstance iotTestFeature: IotTestFeature,
+            @BindsInstance @Named("KeyStorePath") keyStorePath: String,
+            @BindsInstance @Named("KeyStoreName") keyStoreName: String,
+            @BindsInstance @Named("KeyStorePassword") keyStorePassword: String,
+            @BindsInstance @Named("CertificateId") certificateId: String,
+            @BindsInstance @Named("IotPolicyName") policyName: String
         ): IotTestComponent
     }
 
     fun getIotTestFeature(): IotTestFeature
     fun inject(activity: RequestActivity)
+    val messagingJob: MessagingJob
 }
